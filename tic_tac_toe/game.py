6139507f1f14a9d6e6f0f1eb7d169d36af7d1e6d
Nick McDaniel <nickamcd@gmail.com>
2021-06-10 13:42:42 -0700

    Added minimax AI and simulation

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,4 +1,4 @@
-from player import HumanPlayer, RandomComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, MinMaxComputerPlayer
 
 class TicTacToe:
   def __init__(self):
@@ -92,7 +92,20 @@ def play(game, x_player, o_player, print_game=True):
     print('It\'s a tie!')
 
 if __name__ == '__main__':
-  x_player = HumanPlayer('X')
-  o_player = RandomComputerPlayer('O')
-  t = TicTacToe()
-  play(t, x_player, o_player, print_game=True)
+  x_wins = 0
+  o_wins = 0
+  ties = 0
+
+  for i in range(100):
+    x_player = RandomComputerPlayer('X')
+    o_player = MinMaxComputerPlayer('O')
+    t = TicTacToe()
+    result = play(t, x_player, o_player, print_game=False)
+    if result == 'X':
+      x_wins += 1
+    elif result == 'O':
+      o_wins += 1
+    else:
+      ties += 1
+
+  print(f'After 100 iterations, we see {x_wins} X wins, {o_wins} O wins, and {ties} ties.')
--- tic_tac_toe/player.py
+++ tic_tac_toe/player.py
@@ -35,3 +35,60 @@ class HumanPlayer(Player):
         print('Invalid square. Try again.')
     
     return value
+
+class MinMaxComputerPlayer(Player):
+  def __init__(self, mark):
+    super().__init__(mark)
+
+  def get_move(self, game):
+    if len(game.available_moves()) == 9:
+      cell = random.choice(game.available_moves()) # random choice if all spaces empty
+    else:
+      cell = self.minimax(game, self.mark)['position']
+    return cell
+
+  def minimax(self, state, player):
+    max_player = self.mark
+    other_player = 'O' if player == 'X' else 'X'
+
+    # base case
+    if state.current_winner == other_player:
+      return {
+        'position': None,
+        'score': 1 * (state.num_empty_cells() + 1) if other_player == max_player else -1 * (state.num_empty_cells() + 1)
+      }
+    elif not state.empty_cells(): # no winner
+      return {
+        'position': None,
+        'score': 0
+      }
+    
+    if player == max_player:
+      best = {
+        'position': None,
+        'score': -math.inf # score should maximize
+      }
+    else:
+      best = {
+        'position': None,
+        'score': math.inf # score should minimize
+      }
+
+    for possible_move in state.available_moves():
+      # make move
+      state.make_move(possible_move, player)
+      # recurse using minimax to simulate game
+      sim_score = self.minimax(state, other_player) # alternate player
+      # undo move
+      state.board[possible_move] = ' '
+      state.current_winner = None
+      sim_score['position'] = possible_move
+      # update dictionaries if necessary
+      if player == max_player:
+        if sim_score['score'] > best['score']:
+          best = sim_score
+      else:
+        if sim_score['score'] < best['score']:
+          best = sim_score
+
+    return best
